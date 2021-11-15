e9ed4942f7a648355df8efeace58d0c647944995
ismael123 <ismael123@DESKTOP-TDIC8OV>
2021-11-15 16:09:11 +0100

    Vlidacion de los datos con el archivo ussuarios.txt

--- src/main/java/clases/SistemaPrincipal.java
+++ src/main/java/clases/SistemaPrincipal.java
@@ -4,6 +4,9 @@
  */
 package clases;
 import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,30 +18,67 @@ public class SistemaPrincipal {
         boolean validar= false;
         Scanner sc= new Scanner(System.in);
         System.out.println("Inicio del sistema");
-        while(validar==false){
+        while(validar==false){ //se pone un ciclo para que cuando ponga mal los datos ingrese denuevo
         System.out.println("Ingrese el usuario y contraseña");
         System.out.println("usuario: ");
         String Usuario= sc.nextLine();
         System.out.println("Contraseña: ");
         String Contraseña= sc.nextLine();
-        if(validardatos(Usuario)==false || validardatos(Contraseña)==false){
+        if( IngresoSistema(Usuario.toLowerCase(),Contraseña.toLowerCase(),"usuarios.txt")==false || validardatos(Usuario)==false){
             System.out.println("credenciales no validas");
+           
           
         }else{
             System.out.println("Bienvenido al sistema");
             validar=true;
         }
-      
         }
         
     }
     public static boolean validardatos(String datos){
-       return datos.matches("[a-zA-z]*");
-        
-        
-        
-        
-        
+       return datos.matches("[a-zA-z]*");  
+    }
+    private static boolean IngresoSistema(String user, String contraseña, String nombrearchivo){
+     boolean encontrado= false;  
+     File archivo = null;
+     FileReader fr = null;
+    BufferedReader br = null;
+     String linea;
+     String Encabezado;
+     try {
+         archivo=new File(nombrearchivo);
+         fr= new FileReader(archivo,StandardCharsets.UTF_8);
+         br=new BufferedReader(fr);
+         Encabezado=br.readLine();
+        while ((linea = br.readLine()) != null )  {
+           String []datos;
+           datos=linea.split(",");
+           String usuario=datos[3];
+           String contra=datos[4]; 
+           if(usuario.equals(user) && contraseña.equals(contra)){
+            encontrado=true; 
+           }   
+         }    
+     }catch(Exception e){
+         e.printStackTrace();
+     }
+     finally {
+            
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+     if(encontrado==true){
+         return true;
+     }
+     else{
+         return false;
+     }
+         
     }
   
     
