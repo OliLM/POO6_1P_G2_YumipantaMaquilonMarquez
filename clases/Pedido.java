501ce8c12b9582affa262b5bfbd605003c5d2c1d
Janeth Molina <127567902+JulissaMC@users.noreply.github.com>
2023-11-21 14:53:25 -0500

    Tarea SOLID
    cambios

---
+++ clases/Pedido.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author DELL
+ */
+public class Pedido {
+    private String codigo;
+    private String nombre;
+    private ArrayList<Plato> carrito;
+    
+    public Pedido(String nombre){
+        this.codigo=String.valueOf((int)(Math.random()*1000000));;
+        this.nombre=nombre;
+        this.carrito= new ArrayList<>();
+    }
+    
+    public String getcodigo(){
+        return codigo;
+    }
+    public void setcodigo(String codigo){
+        this.codigo=codigo;
+    }
+
+    public ArrayList<Plato> getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(ArrayList<Plato> carrito) {
+        this.carrito = carrito;
+    }
+    
+    
+    public String getnombre(){
+        return nombre;
+    }
+    public void setnombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "\ncodigo: "+codigo+"\nnombre: "+ nombre+"\nSu pedido: " + carrito;
+    }
+    
+    
+            
+}
